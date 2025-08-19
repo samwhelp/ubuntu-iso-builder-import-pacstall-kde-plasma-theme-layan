410ac0f3fca0cf34fa73d9f447fd5a35308f9ff7
samwhelp <samwhelp@users.noreply.github.com>
2025-08-19 16:45:15 +0800

    fix

--- profile/template/cycle/helper/stage/module/pacstall-themes-install/install.sh
+++ profile/template/cycle/helper/stage/module/pacstall-themes-install/install.sh
@@ -81,6 +81,8 @@ sys_pacstall_install_packages () {
 
 local run_cmd=$(cat << __EOF__
 	pacstall -QPINs
+		bean-resource-kde-plasma-layout-templates
+
 		bean-appearance-citrus-icon-theme
 		bean-appearance-layan-gtk-theme
 		bean-appearance-layan-kde-theme
